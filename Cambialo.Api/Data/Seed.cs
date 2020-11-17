d447071b51eeeee0adf6cfd33bdd70882cfa0f29
lavilaso <avilaluis0@hotmail.com>
2020-11-16 21:36:43 -0500

    Agragado Seeding

---
+++ Cambialo.Api/Data/Seed.cs
@@ -0,0 +1,87 @@
+﻿using Cambialo.Api.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cambialo.Api.Data
+{
+    public class Seed
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole<Guid>> roleManager,
+            ApplicationDbContext dbContext)
+        {
+            try
+            {
+                await dbContext.Database.EnsureCreatedAsync();
+            }
+            catch (Exception ex)
+            {
+
+                Debug.WriteLine(ex.Message);
+            }
+
+            if (!userManager.Users.Any())
+            {
+                var user1 = GenerateUser("Luis", "Avila", "avilaluis0@hotmail.com", DateTime.UtcNow);
+                var user2 = GenerateUser("Juan", "Avila", "ultron_lmn4@hotmail.com", DateTime.UtcNow);
+
+                try
+                {
+                    await userManager.CreateAsync(user1, "123456");
+                    await userManager.CreateAsync(user2, "123456");
+                }
+                catch (Exception ex)
+                {
+
+                    Debug.WriteLine(ex.Message);
+                }
+
+                await dbContext.Articles.AddAsync(GenerateArticle("Celular Moto G 4 Play",
+                    "Celular Moto G 4 Play en excelente estado y 4 años de uso.",
+                    user1));
+                await dbContext.Articles.AddAsync(GenerateArticle("Impresora HP Disject 3050",
+                    "Impresora multifuncional con 5 años en excelente estado.",
+                    user2));
+
+                try
+                {
+                    await dbContext.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+        }
+
+
+        private static User GenerateUser(string firtsName, string lastName, string email, DateTime registrationDate)
+        {
+            return new User()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = firtsName,
+                LastName = lastName,
+                Email = email,
+                UserName = email,
+                RegistrationDate = registrationDate
+            };
+        }
+
+        private static Article GenerateArticle(string name, string description, User user)
+        {
+            return new Article()
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Description = description,
+                Status = ArticleStatus.Available,
+                User = user
+            };
+        }
+    }
+}
--- Cambialo.Api/Program.cs
+++ Cambialo.Api/Program.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cambialo.Api.Data;
+using Cambialo.Api.Data.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +15,31 @@ namespace Cambialo.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var hostingEnvironment = services.GetService<IWebHostEnvironment>();
+
+                //TODO: Probablemente la mejor opcion sea agregar las migraciones antes de el seeding, es necesario migrar en dev y produccion
+
+                if (hostingEnvironment.IsDevelopment())
+                {
+                    var userManager = services.GetRequiredService<UserManager<User>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+                    var dbContext = services.GetRequiredService<ApplicationDbContext>();
+
+                    await Seed.InitializeAsync(userManager, roleManager, dbContext);
+                }
+                else if (hostingEnvironment.IsProduction())
+                {
+                    //TODO: Agregar logica para realizar migraciones al pasar a produccion
+                }
+            }
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
